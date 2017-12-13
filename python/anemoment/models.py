1c9051a89837c61d1d474c2fe22379c205ea4517
Adam <PseudoDesign@users.noreply.github.com>
2017-12-12 23:57:31 -0700

    first step of the model

--- python/anemoment/models.py
+++ python/anemoment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class WindDataPoint(models.Model):
+    X_AXIS = "X"
+    Y_AXIS = "Y"
+    Z_AXIS = "Z"
+    AXIS_CHOICES = (
+        (X_AXIS, "X-Axis"),
+        (Y_AXIS, "Y-Axis"),
+        (Z_AXIS, "Z-Axis"),
+    )
+    axis = models.CharField(max_length=2, choices=AXIS_CHOICES)
--- python/anemoment/tests.py
+++ python/anemoment/tests.py
@@ -1,9 +1,2 @@
 from django.test import TestCase
 
-user = 'vagrant'
-pw_file = '/share/.mysql_user_pass'
-with open(pw_file) as f:
-    password = f.read().split()[0]
-host = 'localhost'
-port = 3306
-db_name = 'anemomenttest'
