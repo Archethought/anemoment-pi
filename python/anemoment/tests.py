f467b0826a1749c2498a880f26313c391370eabd
Adam <PseudoDesign@users.noreply.github.com>
2017-12-12 23:04:25 -0700

    starting tests

--- python/anemoment/tests.py
+++ python/anemoment/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+user = 'vagrant'
+pw_file = '/share/.mysql_user_pass'
+with open(pw_file) as f:
+    password = f.read().split()[0]
+host = 'localhost'
+port = 3306
+db_name = 'anemomenttest'
