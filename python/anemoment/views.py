5fcd68464c8c88b031fe761e1a9ee8de4ea917ca
Adam <PseudoDesign@users.noreply.github.com>
2017-12-16 22:27:35 -0700

    reworked stream

--- python/anemoment/views.py
+++ python/anemoment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.core import serializers
+import random
+from django.utils import timezone
+from datetime import timedelta
 
 from .models import WindData
 
@@ -9,5 +11,11 @@ def graph(request):
     return render(request, 'anemoment/graph.html')
 
 def wind_data(request):
-    model_data = list(WindData.objects.all().values())
+    threshold = timezone.now() - timedelta(minutes=1)
+    model_data = list(WindData.objects.filter(timestamp__gte=threshold).values())
+    WindData.objects.create(
+        x_speed=random.uniform(-1, 1),
+        y_speed=random.uniform(-1, 1),
+        z_speed=random.uniform(-1, 1),
+    )
     return JsonResponse(model_data, safe=False)
