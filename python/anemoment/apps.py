df60c9a3947ebbcc1a9c4d474220d700593706ae
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-23 16:14:35 -0700

    added ready method

--- python/anemoment/apps.py
+++ python/anemoment/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class AnemomentConfig(AppConfig):
     name = 'anemoment'
+
+    def ready(self):
+        """
+        Runs on application startup.
+        """
+        print("Hello World")
