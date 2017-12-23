ced13919decff62c5814bb2414dedc3eb4106fbb
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-23 16:35:18 -0700

    added pyserial

---
+++ python/anemoment/parser.py
@@ -0,0 +1,15 @@
+class Parser:
+    """
+    Parses input from an Anemoment TriSonica.
+    """
+
+    def __init__(self, serial):
+        """
+        :param serial: The open serial connection to parse
+        :type serial: serial.Serial
+        """
+        pass
+
+
+
+
