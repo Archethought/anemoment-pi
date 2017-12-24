63bb574e9a248f513ddf4668e9dc70ffa52eb2ec
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-23 22:32:54 -0700

    working parsing test

--- python/anemoment/parser.py
+++ python/anemoment/parser.py
@@ -1,17 +1,26 @@
+from .models import WindData
+
+
 class Parser:
     """
     Parses input from an Anemoment TriSonica.
     """
 
-    def __init__(self, serial):
+    def __init__(self, serial=None):
         """
-        :param serial: The open serial connection to parse
+        :param serial: A serial connection to continuously parse
         :type serial: serial.Serial
         """
         self.__serial = serial
-        if not self.__serial.is_open:
+        if self.__serial is not None and not self.__serial.is_open:
             self.__serial.open()
 
+    def parse_string(self, data):
+        """
+        :param data: A line of Anemoment TriSonica formatted data
+        :type data: string
+        """
+        pass
 
 
 
--- python/anemoment/tests.py
+++ python/anemoment/tests.py
@@ -5,6 +5,20 @@ from .parser import Parser
 
 
 class TestParser(TestCase):
+    GOOD_ANEMOMENT_DATA = [
+        {
+            "raw_input": "S 05.2 D 112 U -01.9 V 04.7 W 01.1 T 22.6",
+            "parsed_data": {
+                "speed": 5.2,
+                "direction": 112,
+                "north_south": -1.9,
+                "west_east": 4.7,
+                "up_down": 1.1,
+                "temperature": 22.6,
+            }
+        },
+    ]
+
     def setUp(self):
         self.mock_serial = Mock()
 
@@ -17,3 +31,10 @@ class TestParser(TestCase):
         self.mock_serial.is_open = True
         Parser(self.mock_serial)
         self.mock_serial.open.assert_not_called()
+
+    @patch("anemoment.parser.WindData")
+    def test_parse_good_anemoment_data(self, model):
+        p = Parser()
+        for e in self.GOOD_ANEMOMENT_DATA:
+            p.parse_string(e["raw_input"])
+            model.objects.create.assert_called_with(**e["parsed_data"])
