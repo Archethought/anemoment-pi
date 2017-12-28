a0a8772206b2bc639add5544090502831dff1431
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-28 14:45:28 -0700

    top level unit tests passing

--- python/static/anemoment/js/spec/GraphSpec.js
+++ python/static/anemoment/js/spec/GraphSpec.js
@@ -1,30 +1,46 @@
 describe("Graph", function () {
     var DATA_URL = 'data_source'
-    var MOCK_DATA = 'mock_data'
+    var MOCK_C3_GRAPH = 'mock_data'
 
     beforeEach(function () {
         this.graph = new Graph(DATA_URL);
-        spyOn
     });
 
     describe("when it is initialized", function () {
-
         it("updates the data url", function() {
-            expect(this.graph.data_url).toBe(DATA_URL)
+            expect(this.graph.data_url).toBe(DATA_URL);
         });
 
+        it("does not render the c3 graph", function() {
+            expect(this.graph.c3_graph).toBe(null);
+        });
     });
-/*
-    describe("when it is rendered", function() {
-       beforeEach(function () {
-           this.graph = new Graph(DATA_URL);
-           spyOn(this.graph, "renderC3");
-           this.graph.render();
-       });
 
-       it("renders a C3 graph with no data", function () {
-           expect(this.graph.renderC3).toHaveBeenCalled();
-       });
+    describe("when it is rendered", function() {
+        beforeEach(function () {
+            spyOn(this.graph, 'renderC3').and.returnValue(MOCK_C3_GRAPH);
+            this.graph.render();
+        });
+
+        it("renders a C3 graph", function () {
+            expect(this.graph.renderC3).toHaveBeenCalled();
+        });
+
+        it("updates the c3_graph parameter", function () {
+            expect(this.graph.c3_graph).toBe(MOCK_C3_GRAPH);
+        });
     });
-*/
+
+    describe("when it is updated", function () {
+        beforeEach(function () {
+            spyOn(this.graph, 'updateC3');
+            this.graph.c3_graph = MOCK_C3_GRAPH;
+            this.graph.update();
+        });
+
+        it("updates the stored c3_graph with data from the stored data_url", function () {
+            expect(this.graph.updateC3).toHaveBeenCalledWith(MOCK_C3_GRAPH, DATA_URL);
+        });
+    });
+
 });
--- python/static/anemoment/js/src/Graph.js
+++ python/static/anemoment/js/src/Graph.js
@@ -16,8 +16,22 @@ Graph.prototype.render = function () {
 };
 
 /**
+ * Updates the graph with the data at data_url
+ */
+Graph.prototype.update = function () {
+    this.updateC3(this.c3_graph, this.data_url);
+};
+
+/**
  * Generates the C3 graph and returns the associated object
  */
 Graph.prototype.renderC3 = function () {
+};
 
+/**
+ * Update the c3 graph
+ * @param {c3 chart} graph - the c3 graph to update
+ * @param {string} data_url - The relative URL of the json-formatted graph data.
+ */
+Graph.prototype.updateC3 = function (graph, data_url) {
 };
