8d2825bf77812d303021b4b64a593af4dbbdde63
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-28 13:40:13 -0700

    fixed 'prototype's

--- python/static/anemoment/js/spec/GraphSpec.js
+++ python/static/anemoment/js/spec/GraphSpec.js
@@ -1,12 +1,30 @@
 describe("Graph", function () {
     var DATA_URL = 'data_source'
+    var MOCK_DATA = 'mock_data'
 
     beforeEach(function () {
-        this.graph = new Graph();
+        this.graph = new Graph(DATA_URL);
+        spyOn
     });
 
-    it("can be initialized with a data source", function() {
-        this.graph.init(DATA_URL)
-        expect(this.graph.data_source).toBe(DATA_URL)
+    describe("when it is initialized", function () {
+
+        it("updates the data url", function() {
+            expect(this.graph.data_url).toBe(DATA_URL)
+        });
+
     });
+/*
+    describe("when it is rendered", function() {
+       beforeEach(function () {
+           this.graph = new Graph(DATA_URL);
+           spyOn(this.graph, "renderC3");
+           this.graph.render();
+       });
+
+       it("renders a C3 graph with no data", function () {
+           expect(this.graph.renderC3).toHaveBeenCalled();
+       });
+    });
+*/
 });
--- python/static/anemoment/js/src/Graph.js
+++ python/static/anemoment/js/src/Graph.js
@@ -1,5 +1,23 @@
-function Graph() {
+/**
+ * Represents a graph object
+ * @param {string} data_url - The relative URL of the json-formatted graph data.
+ * @constructor
+ */
+function Graph(data_url) {
+    this.data_url = data_url;
+    this.c3_graph = null;
 }
-Graph.prototype.init = function(data_source) {
-    this.data_source = data_source;
+
+/**
+ * Renders the graph on the page
+ */
+Graph.prototype.render = function () {
+    this.c3_graph = this.renderC3();
+};
+
+/**
+ * Generates the C3 graph and returns the associated object
+ */
+Graph.prototype.renderC3 = function () {
+
 };
