279bee115529bf4bc68f477f034528544f0116fe
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-12-29 03:43:25 -0700

    graph class is functional

--- python/static/anemoment/js/src/Graph.js
+++ python/static/anemoment/js/src/Graph.js
@@ -8,14 +8,20 @@ class Graph {
         this.data_url = data_url;
         this.c3_graph = null;
         this.bind_to = null;
-        this.parseTime = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
+    }
+
+    parseTime (time) {
+        if (time.includes(".")) {
+            return d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ").parse(time);
+        } else {
+            return d3.time.format("%Y-%m-%dT%H:%M:%SZ").parse(time);
+        }
     }
 
     /**
     * Renders the graph on the page
     */
     render (bind_to) {
-        console.log(this);
         this.bind_to = bind_to;
         this.c3_graph = this.renderC3(bind_to);
     }
@@ -24,19 +30,17 @@ class Graph {
      * Updates the graph with the data at data_url
      */
     update () {
-        console.log(this);
         this.updateC3();
     }
 
     renderC3 (bind_to) {
-        console.log(this);
         this.bind_to = bind_to;
         var graph = this;
         d3.json(this.data_url, function(error, data) {
             if (error) throw error;
 
             data.forEach(function (d) {
-                d.timestamp = graph.parseTime.parse(d.timestamp);
+                d.timestamp = graph.parseTime(d.timestamp);
             });
 
             graph.c3_graph = c3.generate({
@@ -79,13 +83,12 @@ class Graph {
             if (error) throw error;
             var latest_time = 0;
             data.forEach(function(d) {
-                d.timestamp = parseTime.parse(d.timestamp);
+                d.timestamp = parseTime(d.timestamp);
                 if (d.timestamp > latest_time) {
                     latest_time = d.timestamp;
                 }
             });
 
-            console.log(chart);
             var start_time = d3.time.minute.offset(latest_time, -1);
             chart.load({
                 json: data,
