997141847c8ba1ed49c50e7d69efaca5c515b62c
Adam <PseudoDesign@users.noreply.github.com>
2018-01-04 01:47:15 -0700

    added uart

--- python/anemoment/apps.py
+++ python/anemoment/apps.py
@@ -1,11 +1,19 @@
 from django.apps import AppConfig
-
+import serial
 
 class AnemomentConfig(AppConfig):
     name = 'anemoment'
+    perser = None
 
     def ready(self):
         """
         Runs on application startup.
         """
-        print("Hello World")
+        UART = None
+        from .parser import Parser
+
+        if UART is not None:
+            ser = serial.Serial(port=UART, baudrate=115200)
+        else:
+            ser = None
+        self.parser = Parser(ser)
