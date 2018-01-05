8c68fbf7c0591bbacad5b3789adb28162c34f056
PseudoDesign <PseudoDesign@users.noreply.github.com>
2018-01-05 07:49:16 +0000

    running with mariadb

--- python/anemoment/apps.py
+++ python/anemoment/apps.py
@@ -3,3 +3,13 @@ import serial
 
 class AnemomentConfig(AppConfig):
     name = 'anemoment'
+
+    def ready(self):
+        UART = "/dev/ttyAMA0"
+        from .parser import Parser
+
+        if UART is not None:
+            ser = serial.Serial(port=UART, baudrate=115200)
+        else:
+            ser = None
+        self.parser = Parser(ser)
