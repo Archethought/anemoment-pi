33d79abae62b4995c8bd55de701f61c70b126b13
PseudoDesign <PseudoDesign@users.noreply.github.com>
2018-01-05 07:36:26 +0000

    updated parser

--- python/anemoment/apps.py
+++ python/anemoment/apps.py
@@ -3,17 +3,3 @@ import serial
 
 class AnemomentConfig(AppConfig):
     name = 'anemoment'
-
-    def ready(self):
-        """
-        Runs on application startup.
-        """
-        # UART = None
-        UART = "/dev/ttyAMA0"
-        from .parser import Parser
-        print(UART)
-        if UART is not None:
-            ser = serial.Serial(port=UART, baudrate=115200)
-        else:
-            ser = None
-        self.parser = Parser(ser)
